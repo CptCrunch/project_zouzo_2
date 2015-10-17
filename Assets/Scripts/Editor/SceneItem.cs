7780bbeb182bcda0421237a6dcf8625cd7edc059
Mario Kerndler <mini.korni@gmail.com>
2015-10-17 22:51:23 +0200

    Gamerules Script and SceneEditor Scipt

---
+++ Assets/Scripts/Editor/SceneItem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SceneItem : Editor {
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_01")]
+	public static void OpenTestScene_01()
+    {
+        OpenScene("Test Scenes/TestScene_01");
+    }
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_02")]
+    public static void OpenTestScene_02()
+    {
+        OpenScene("Test Scenes/TestScene_02");
+    }
+
+    [MenuItem("Open Scenes/Main Menu")]
+    public static void MainMenu()
+    {
+        OpenScene("MainMenu");
+    }
+
+    static void OpenScene(string name)
+    {
+        if(EditorApplication.SaveCurrentSceneIfUserWantsTo())
+        {
+            EditorApplication.OpenScene("Assets/Scenes/" + name + ".unity");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Gamerules.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gamerules : MonoBehaviour {
+
+    public static Gamerules _instance;
+
+    #region Gamerules Variable
+    [Header("Game Rules")]
+    [Range (1,4)]
+    public int playerAmmount = 1;
+    [Tooltip("Defines the maximal use of an ability")]
+    public uint abilityLimit;
+    public float itemSpawnrate;
+    [Tooltip("Time between player death and spawn")]
+    public float timeDeathSpawn;
+    [Range(0,100)]
+    public float damageModifier;
+    public Gamemode[] gameModeList = new Gamemode[2];
+    #endregion
+
+    void Awake()
+    {
+        if (_instance == null) { _instance = this; }
+    }
+
+    [System.Serializable]
+    public class Gamemode
+    {
+        public string name;
+        public int lifeLimit;
+        [Range(0,100)]
+        public float timeLimit;
+    }
+}
