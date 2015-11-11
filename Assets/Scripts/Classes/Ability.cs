71af6d17f648c74f85a8492450d689545043836e
CptCrunch <mini.korni@gmail.com>
2015-11-11 19:31:34 +0100

    Attack and Ability Class

---
+++ Assets/Scripts/Classes/Ability.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Ability : Attacks {
+
+    public Ability() : base()
+    {
+
+    }
+
+    public override void Use()
+    {
+        throw new NotImplementedException();
+    }
+
+}
