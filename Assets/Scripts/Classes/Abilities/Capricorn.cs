367ab65c53e000eba87d2865489b8ac8301f386f
Mario Kerndler <mini.korni@gmail.com>
2015-11-13 13:24:43 +0100

    Animations Earth

---
+++ Assets/Scripts/Classes/Abilities/Capricorn.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Capricorn : Attacks
+{
+    public Capricorn(string name, float damage, float castTime, float duration, float cooldown, GameObject capricornPrefab) : base(10, name, 0, damage, castTime, duration, cooldown, capricornPrefab) { }
+
+    public override void Use() {}
+}
