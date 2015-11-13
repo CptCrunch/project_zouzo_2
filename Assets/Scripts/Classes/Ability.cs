66b36da1f984941646db13c8451ce344838fc91f
FLOBUG02\Florian <bugiflo@gmail.com>
2015-11-13 11:48:29 +0100

    Attack Class added Gameobject

---
+++ Assets/Scripts/Classes/Ability.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Ability : Attacks {
+
+    public Ability(uint id, string name, float heal, float damage, float castTime, float cooldown) : base(id, name, heal, damage, castTime, cooldown)
+    {
+
+    }
+
+    public override void Use()
+    {
+        throw new NotImplementedException();
+    }
+
+}
---
+++ Assets/Scripts/Editor/SceneItem.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneItem : Editor {
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_01_Movement")]
+	public static void OpenTestScene_01()
+    {
+        OpenScene("Test Scenes/TestScene_01_Movement");
+    }
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_02_Platforms")]
+    public static void OpenTestScene_02()
+    {
+        OpenScene("Test Scenes/TestScene_02_Platforms");
+    }
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_03_Selection")]
+    public static void OpenTestScene_03_1()
+    {
+        OpenScene("Test Scenes/TestScene_03_Selection");
+    }
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_03_AfterSelection")]
+    public static void OpenTestScene_03_2()
+    {
+        OpenScene("Test Scenes/TestScene_03_AfterSelection");
+    }
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_04_Animations")]
+    public static void OpenTestScene_04()
+    {
+        OpenScene("Test Scenes/TestScene_04_Animations");
+    }
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_05_JumpPad")]
+    public static void OpenTestScene_05()
+    {
+        OpenScene("Test Scenes/TestScene_05_JumpPad");
+    }
+
+    [MenuItem("Open Scenes/Test Scenes/TestScene_06_Editor")]
+    public static void OpenTestScene_06()
+    {
+        OpenScene("Test Scenes/TestScene_06_Editor");
+    }
+
+    [MenuItem("Open Scenes/Main Menu")]
+    public static void MainMenu()
+    {
+        OpenScene("MainMenu");
+    }
+
+    static void OpenScene(string name)
+    {
+        if(EditorApplication.SaveCurrentSceneIfUserWantsTo())
+        {
+            EditorApplication.OpenScene("Assets/Scenes/" + name + ".unity");
+        }
+    }
+
+}
