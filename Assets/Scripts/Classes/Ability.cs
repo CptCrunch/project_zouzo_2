0864f4f998223282eaf43e988a35057c22d63efa
David Holy <davidholy@me.com>
2015-11-13 12:09:50 +0100

    Ability Error Fix

--- Assets/Scripts/Classes/Ability.cs
+++ Assets/Scripts/Classes/Ability.cs
@@ -4,7 +4,7 @@ using System;
 
 public class Ability : Attacks {
 
-    public Ability(uint id, string name, float heal, float damage, float castTime, float cooldown) : base(id, name, heal, damage, castTime, cooldown)
+    public Ability(uint id, string name, float heal, float damage, float castTime, float duration, float cooldown, GameObject abilityPrefab) : base(0, name, heal, damage, castTime, duration, cooldown, abilityPrefab)
     {
 
     }
