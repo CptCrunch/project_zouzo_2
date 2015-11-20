d0d952b6e137e3b6f1001b0dff93e2920a5c6a74
FLOBUG02\Florian <bugiflo@gmail.com>
2015-11-20 11:20:33 +0100

    Zusammenf?gung

--- Assets/Scripts/Manager/AbilityManager.cs
+++ Assets/Scripts/Manager/AbilityManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class AbilityManager : MonoBehaviour {
 
     public static AbilityManager _instance;
-    public GameObject[] abilityCollider = new GameObject[13];
 
     public Abilities[] abilities = new Abilities[0];
     void Awake()
@@ -13,16 +12,21 @@ public class AbilityManager : MonoBehaviour {
     }
 
     public Attacks UseCapricorn() {
-        return new Capricorn(abilities[9].damage, abilities[9].castTime, abilities[9].duration, abilities[9].cooldown, abilities[9].prefab);
+        return new Capricorn(abilities[9].damage, abilities[9].castTime, abilities[9].duration, abilities[9].cooldown, abilities[9].range);
+    }
+
+    public Attacks Basic()
+    {
+        return new Basic(abilities[12].damage, abilities[12].castTime, abilities[12].duration, abilities[12].cooldown, abilities[12].range);
     }
 }
 
 [System.Serializable]
 public class Abilities {
     public string name;
-    public GameObject prefab;
     public float damage;
     public float castTime;
     public float duration;
     public float cooldown;
+    public float range;
 }
