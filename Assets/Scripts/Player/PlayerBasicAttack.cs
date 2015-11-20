648cdf56b9ca6c5470e3bfad7a2479f0faf2fb82
CptCrunch <mini.korni@gmail.com>
2015-11-20 20:12:25 +0100

    Home PC commit

---
+++ Assets/Scripts/Player/PlayerBasicAttack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBasicAttack : MonoBehaviour {
+
+    private BoxCollider2D collider;
+    private bool attacking;
+
+	void Awake () {
+        collider = GetComponent<BoxCollider2D>();
+	}
+	
+	void OnCollisionStay2D(Collision2D other)
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            other.gameObject.GetComponent<Player>().playerVitals.GetDamage(GetComponentInParent<Player>().basicAttackDamage);
+            print("Attack");
+        }
+    }
+
+    private IEnumerator Attack(float _timeBetweenAttacks)
+    {
+        attacking = true;
+        yield return new WaitForSeconds(_timeBetweenAttacks);
+        attacking = false;
+    }
+}
