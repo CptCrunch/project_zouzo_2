5219370458f606afa8be7060e56c2004422d8530
Mario Kerndler <mini.korni@gmail.com>
2015-11-20 09:41:32 +0100

    Basic Attack #1

---
+++ Assets/Scripts/Classes/Abilities/Capricorn.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Capricorn : Attacks
+{
+    public Capricorn(float damage, float castTime, float duration, float cooldown, GameObject capricornPrefab) : base(10, "capricorn", 0, damage, castTime, duration, cooldown, capricornPrefab) { }
+
+    public override void Use() {}
+}
---
+++ Assets/Scripts/Player/PlayerBasicAttack.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBasicAttack : MonoBehaviour {
+    public void Attack()
+    {
+        
+    }
+}
