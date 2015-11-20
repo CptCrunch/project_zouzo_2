ff20d9992fedce2d65c66bd0f9de9f6fb895dcd2
FLOBUG02\Florian <bugiflo@gmail.com>
2015-11-20 20:12:01 +0100

    button rework

---
+++ Assets/Scripts/Classes/Abilities/Capricorn.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Capricorn : Attacks {
+
+    public Capricorn(float damage, float castTime, float duration, float cooldown, float range) : base(
+        10, "capricorn", "CC", true, 0, damage, castTime, duration, cooldown, range) { }
+
+    public override void Use(GameObject _target) {
+    }
+}
