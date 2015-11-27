4f03278b36cb51059fd5d5dc64931c8728ae29ca
FLOBUG02\Florian <bugiflo@gmail.com>
2015-11-27 11:34:29 +0100

    button imput change, basic fix and capricorn deals damage (cc does not work)

---
+++ Assets/Scripts/Classes/Util.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Util{
+
+    public static float Ease(float x, float easeAmount)
+    {
+        float a = easeAmount + 1;
+        return Mathf.Pow(x, a) / (Mathf.Pow(x, a) + Mathf.Pow(1 - x, a));
+    }
+}
