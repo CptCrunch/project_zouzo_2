5adcd10132431b5f2659631144cb24d9fa7843e7
Mario Kerndler <mini.korni@gmail.com>
2015-12-01 09:12:47 +0100

    Threads

---
+++ Assets/Scripts/Classes/Abilities/Capricorn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Capricorn : Attacks {
+
+    public Capricorn(float damage, float castTime, float duration, float cooldown, float range) : base(
+        10, "capricorn", "CC", true, 0, damage, castTime, duration, cooldown, range) { }
+
+    public override void Use(GameObject _target) {
+        
+        // get the vitals of the target
+        LivingEntity Vitals = _target.GetComponent<Player>().playerVitals;
+
+        // kock the target up and deal damage
+        try {
+            Vitals.ApplyPlayerKnockUp(10f, Convert.ToInt32(Util.ConvertSecondsToMilliseconds(2)));
+        }
+        catch (OverflowException)
+        {
+            Debug.LogError("Is Outside range of Int32 tyoe.");
+        }
+
+        Vitals.GetDamage(Damage);
+
+        // set the spell onto cooldown
+        OnCooldown = true;
+    }
+}
