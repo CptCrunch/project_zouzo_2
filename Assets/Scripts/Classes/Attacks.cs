55594447e9cf37283cfcd69d2f530fe02d1f5780
Maria <raser.maria@gmail.com>
2015-12-03 11:39:03 +0100

    Graphic Test

---
+++ Assets/Scripts/Classes/Attacks.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Attacks {
+
+    private uint id;
+    private string name;
+    private string type;
+    private float heal;
+    private float damage;
+    private float castTime;
+    private float delay;
+    private float duration;
+    private float cooldown;
+    private bool onCooldown = false;
+    private uint durability;
+    private float range;
+    private bool isMeele;
+    private bool isAOE;
+
+    //Empty Constructor
+    public Attacks() { }
+    
+    //Constructor with Heal
+    public Attacks(uint id, string name, string type, bool isMeele, bool isAOE, float heal, float damage, float castTime, float delay, float duration, float cooldown, float range)
+    {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.isMeele = isMeele;
+        this.isAOE = isAOE;
+        this.heal = heal;
+        this.damage = damage;
+        this.castTime = castTime;
+        this.delay = delay;
+        this.castTime = castTime;
+        this.cooldown = cooldown;
+        this.range = range;
+        if(Gamerules._instance.abilityLimit != 0) { durability = Gamerules._instance.abilityLimit; }
+
+    }
+
+    #region Get & Set 
+    public uint ID { get { return id; } }
+    public string Name { get { return name; } }
+    public string Type { get { return type; } }
+    public bool IsMeele { get { return isMeele; } }
+    public bool IsAOE { get { return isAOE; } }
+    public float Heal { get { return heal; } set { this.heal = value; } }
+    public float Damage { get { return damage; } set { this.damage = value; } }
+    public float CastTime { get { return castTime; } set { this.castTime = value; } }
+    public float Delay { get { return delay;  } }
+    public float Cooldown { get { return cooldown; } set { this.cooldown = value; } }
+    public bool OnCooldown { get { return onCooldown; } set { onCooldown = value; } }
+    public uint Durability { get { return durability; } }
+    public float Range { get { return range; } }
+    #endregion
+
+    public abstract void Use(GameObject _target);
+}
---
+++ Assets/Scripts/Manager/Gamerules.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Gamerules : MonoBehaviour {
+
+    public static Gamerules _instance;
+
+    #region Gamerules Variable
+    [Header("Game Rules")]
+
+    [Range (0,4)]
+    public int playerAmmount = 0;
+
+    public float playerMaxHealth = 0;
+
+    [Tooltip("Defines the maximal use of an ability")]
+    public uint abilityLimit;
+
+    public float itemSpawnrate;
+
+    [Tooltip("Time between player death and spawn")]
+    public float timeDeathSpawn;
+
+    [Range(0,200), Tooltip("In Percent (100% = normal Damage")]
+    public float damageModifier;
+
+    public Gamemode[] gameModeList = new Gamemode[2];
+
+    // Player/Controller Selection
+    [Header("Player Selection")]
+    public GameObject[] playerPrefab = new GameObject[4];
+    private GameObject[] playerSpawn = new GameObject[4];
+    [Tooltip("Spawnpoints must have this given tag")]
+    public string spawnTag;
+
+    // Testing 
+    [HideInInspector]
+    public List<string> connectedControllers = new List<string>();
+    public ButtonLayout buttonLayout;
+    #endregion
+
+    void Awake() {
+        // Dont destroy this object on loading a new level
+        DontDestroyOnLoad(gameObject);
+
+        if (_instance == null) { _instance = this; }
+    }
+    
+    // Create All Player when the level loads
+    void OnLevelWasLoaded() {
+
+        //Get all objecticts with the given tag
+        playerSpawn = GameObject.FindGameObjectsWithTag(spawnTag);
+
+        // instantiate Player-Prefabs for each listed Player on a Spawnpoint
+        if (playerAmmount > 0) {
+            for (int i = 0; i < playerAmmount; i++) {
+                GameObject go = Instantiate(playerPrefab[i], playerSpawn[Random.Range(0, playerSpawn.Length)].transform.position, Quaternion.identity) as GameObject;
+
+                // set the axis of the Prefab
+                if (PlayerSelection._instance.controller[i] == "KB") {
+                    ChangeAxis("KB", go); }
+
+                if(PlayerSelection._instance.controller[i] == "P1") {
+                    ChangeAxis("P1", go); }
+
+                if(PlayerSelection._instance.controller[i] == "P2") {
+                    ChangeAxis("P2", go); }
+
+                if(PlayerSelection._instance.controller[i] == "P3") {
+                    ChangeAxis("P3", go); }
+
+                if(PlayerSelection._instance.controller[i] == "P4") {
+                    ChangeAxis("P4", go); }
+            }
+        }
+    }
+
+    private void ChangeAxis(string axis, GameObject obj) {
+
+        // Change the player axis and name
+        obj.GetComponent<Player>().playerAxis = axis;
+        obj.name = axis + "_Player";
+    }
+
+    [System.Serializable]
+    public class Gamemode
+    {
+        public string name;
+        public int lifeLimit;
+        [Range(0,100)]
+        public float timeLimit;
+    }
+
+
+    [System.Serializable]
+    public class ButtonLayout
+    {
+        public string jump;
+        public string basicAttack;
+        public string ability_1;
+        public string ability_2;
+        public string ability_3;
+    }
+}
