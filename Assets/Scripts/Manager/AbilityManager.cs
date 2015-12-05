fc9295eb1b7f965d7dd995ee4a2eb791c7bfe4e1
FLOBUG02\Florian <bugiflo@gmail.com>
2015-12-05 17:36:19 +0100

    leo spell added

--- Assets/Scripts/Manager/AbilityManager.cs
+++ Assets/Scripts/Manager/AbilityManager.cs
@@ -11,13 +11,18 @@ public class AbilityManager : MonoBehaviour {
         if (_instance == null) { _instance = this; }
     }
 
+    public Attacks CreateLeo()
+    {
+        return new Leo(abilities[4].damage, abilities[4].castTime, abilities[4].travelTime, abilities[4].duration, abilities[4].cooldown, abilities[4].range, abilities[4].targets, abilities[4].charges);
+    }
+
     public Attacks CreateCapricorn() {
-        return new Capricorn(abilities[9].damage, abilities[9].castTime, abilities[9].delay, abilities[9].duration, abilities[9].cooldown, abilities[9].range, abilities[9].knockUpHeght, abilities[9].time);
+        return new Capricorn(abilities[9].damage, abilities[9].castTime, abilities[9].travelTime, abilities[9].duration, abilities[9].cooldown, abilities[9].range, abilities[9].targets, abilities[9].knockUpHeght, abilities[9].time);
     }
 
     public Attacks CreateBasic()
     {
-        return new Basic(abilities[12].damage, abilities[12].castTime, abilities[12].delay, abilities[12].duration, abilities[12].cooldown, abilities[12].range);
+        return new Basic(abilities[12].damage, abilities[12].castTime, abilities[12].travelTime, abilities[12].duration, abilities[12].cooldown, abilities[12].range, abilities[12].targets);
     }
 }
 
@@ -25,15 +30,24 @@ public class AbilityManager : MonoBehaviour {
 public struct Abilities {
     public string name;
     public float damage;
+    [Tooltip("[time in seconds]")]
     public float castTime;
-    public float delay;
+    [Tooltip("the time that the spell needs to reach max range (0 is instand) [time in seconds]")]
+    public float travelTime;
     public float duration;
+    [Tooltip("[time in seconds]")]
     public float cooldown;
     public float range;
+    [Tooltip("amount of players that the spell can hit (0 is infinity)")]
+    public int targets;
 
     [Header("Class specific variables")]
+    [Tooltip("nessesary for: capricorn")]
     public float knockUpHeght;
+    [Tooltip("nessesary for: capricorn")]
     public float knockBackDistance;
-    [Tooltip("time in milliseconds (1:1000)")]
+    [Tooltip("nessesary for: capricorn \n[time in milliseconds (1:1000)]")]
     public int time;
+    [Tooltip("nessesary for: leo")]
+    public int charges;
 }
