6d4ddb898d13357dde814dbc15cd5d582d92baa1
Mario Kerndler <mini.korni@gmail.com>
2015-12-17 09:01:34 +0100

    Anim Fix

--- Assets/Scripts/Manager/Gamerules.cs
+++ Assets/Scripts/Manager/Gamerules.cs
@@ -37,7 +37,8 @@ public class Gamerules : MonoBehaviour {
     // Testing 
     [HideInInspector]
     public List<string> connectedControllers = new List<string>();
-    public ButtonLayout buttonLayout;
+    [HideInInspector]
+    public string[] chosenCharacters = new string[4];
     #endregion
 
     void Awake() {
@@ -50,31 +51,70 @@ public class Gamerules : MonoBehaviour {
     // Create All Player when the level loads
     void OnLevelWasLoaded() {
 
+        int character = 0;
+        int counter = 0;
+
         //Get all objecticts with the given tag
         playerSpawn = GameObject.FindGameObjectsWithTag(spawnTag);
 
         // instantiate Player-Prefabs for each listed Player on a Spawnpoint
-        if (playerAmmount > 0) {
-            for (int i = 0; i < playerAmmount; i++) {
-                GameObject go = Instantiate(playerPrefab[i], playerSpawn[Random.Range(0, playerSpawn.Length)].transform.position, Quaternion.identity) as GameObject;
+        if (playerAmmount > 0)
+        {
+            foreach (string z in chosenCharacters)
+            {
+                if (z == "Earth")
+                {
+                    character = counter;
+                    print(character);
+                }
+                else if (z == "Jupiter")
+                {
+                    character = counter;
+                    print(character);
+                }
+                else if (z == "Saturn")
+                {
+                    character = counter;
+                    print(character);
+                }
+                else if (z == "Sun")
+                {
+                    character = counter;
+                    print(character);
+                }
+
+                GameObject go = Instantiate(playerPrefab[character], playerSpawn[Random.Range(0, playerSpawn.Length)].transform.position, Quaternion.identity) as GameObject;
 
                 // set the axis of the Prefab
-                if (PlayerSelection._instance.controller[i] == "KB") {
-                    ChangeAxis("KB", go); }
+                if (PlayerSelection._instance.controller[counter] == "KB")
+                {
+                    ChangeAxis("KB", go);
+                }
 
-                if(PlayerSelection._instance.controller[i] == "P1") {
-                    ChangeAxis("P1", go); }
+                if (PlayerSelection._instance.controller[counter] == "P1")
+                {
+                    ChangeAxis("P1", go);
+                }
 
-                if(PlayerSelection._instance.controller[i] == "P2") {
-                    ChangeAxis("P2", go); }
+                if (PlayerSelection._instance.controller[counter] == "P2")
+                {
+                    ChangeAxis("P2", go);
+                }
 
-                if(PlayerSelection._instance.controller[i] == "P3") {
-                    ChangeAxis("P3", go); }
+                if (PlayerSelection._instance.controller[counter] == "P3")
+                {
+                    ChangeAxis("P3", go);
+                }
 
-                if(PlayerSelection._instance.controller[i] == "P4") {
-                    ChangeAxis("P4", go); }
+                if (PlayerSelection._instance.controller[counter] == "P4")
+                {
+                    ChangeAxis("P4", go);
+                }
+
+                counter++;
             }
         }
+        
     }
 
     private void ChangeAxis(string axis, GameObject obj) {
