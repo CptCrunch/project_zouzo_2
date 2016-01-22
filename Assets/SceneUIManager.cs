27e63f11cee5a8e64bbf06b68dd7d2a24c47170e
David Holy <davidholy@me.com>
2016-01-22 11:25:50 +0100

    repaired MainMenu animations

---
+++ Assets/SceneUIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneUIManager : MonoBehaviour {
+
+    [Header("Debug")]
+    public DebugValues Tags;
+
+    void Start()
+    {
+        Tags.ResetValues();
+        CustomDebug.Active = Tags.Active;
+        CustomDebug.EnableTag("Main", Tags.Main);
+        CustomDebug.EnableTag("Player", Tags.Player);
+        CustomDebug.EnableTag("Damage", Tags.Damage);
+        CustomDebug.EnableTag("Spells", Tags.Spells);
+        CustomDebug.EnableTag("UI", Tags.UI);
+        CustomDebug.EnableTag("Testing", Tags.Testing);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
