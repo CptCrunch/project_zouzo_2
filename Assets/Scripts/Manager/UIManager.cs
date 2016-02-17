38f81fddec8c847512aa13eba235fe9a17228c95
bugiflo <bugiflo@gmail.com>
2016-02-17 09:20:20 +0100

    ui stuff i gues

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    private static UIManager instance;
+
+    public Vector2[] uiPositions = new Vector2[4];
+    public GameObject uiPrefab;
+
+    private Image[] characterHolder = new Image[4];
+    public Sprite[] playerIcons = new Sprite[4];
+    public Sprite[] playerDeathIcons = new Sprite[4];
+
+    void Awake()
+    {
+        if (instance == null) { instance = this; }
+        else { Debug.LogError("UIManager has already been instantiated"); }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        // #get all player on the stage (gamerules)
+        // #create uiPrefabs for each one
+        // #set player Icons
+
+        // --- [ set player Icons ] ---
+        for (int i = 0; i < characterHolder.Length; i++)
+        {
+            // check if objet isn't null
+            if (characterHolder[i] != null) { characterHolder[i].overrideSprite = playerIcons[i]; }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public UIManager Instance { get { return instance; } }
+}
