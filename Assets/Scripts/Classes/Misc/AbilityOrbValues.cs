77c175862044b487d6f9ddbe2b1610df720456cf
Mario Kerndler <mini.korni@gmail.com>
2016-02-18 08:28:02 +0100

    Moving Things

---
+++ Assets/Scripts/Classes/Misc/AbilityOrbValues.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityOrbValues {
+
+    private GameObject spawnPoint;
+    private GameObject orbPrefab;
+    private bool active;
+
+    public AbilityOrbValues(GameObject spawnPoint, GameObject orbPrefab)
+    {
+        this.spawnPoint = spawnPoint;
+        this.orbPrefab = orbPrefab;
+        active = false;
+    }
+
+    public GameObject SpawnPoint { get { return spawnPoint; } set { this.spawnPoint = value; } }
+    public GameObject OrbPrefab { get { return orbPrefab; } set { orbPrefab = value; } }
+    public bool Active { get { return active; } set { active = value; } }
+}
