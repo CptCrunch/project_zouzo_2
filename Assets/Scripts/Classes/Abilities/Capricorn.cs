9932348d2a9a6d3b9fae350749ce669f275ddce5
CptCrunch <mini.korni@gmail.com>
2016-02-22 21:32:56 +0100

    Spawningn and Death

---
+++ Assets/Scripts/Classes/Abilities/Capricorn.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Capricorn : Attacks {
+
+    private float height;
+    private float knockBackRange;
+    private bool usedInAir = false;
+    private float knockBackStrenght;
+    private bool airCast = false;
+
+    public Capricorn(GameObject caster, float damage, float castTime, float travleTime, float duration, float cooldown, float range, int targets, uint spellDir, float height, float knockBackRange, float knockBackStrenght) : base(
+        caster, 10, "capricorn", "CC", targets, damage, castTime, travleTime, duration, cooldown, range, spellDir)
+    {
+        this.height = height;
+        this.knockBackRange = knockBackRange;
+        this.knockBackStrenght = knockBackStrenght;
+    }
+
+    public float KnockBackDistance { get { return knockBackRange; } }
+    public float KnockBackStrenght { get { return knockBackStrenght; } }
+    public bool UsedInAir { get { return usedInAir; } }
+
+    public override void StartSpell()
+    {
+        //check if knockUp should be used
+        if (Caster.GetComponent<Controller2D>().collisions.below)
+        {
+            if (!IsDisabled)
+            {
+                // set spell as started
+                IsStarted = true;
+
+                // set animation
+                Caster.GetComponent<Animator>().SetTrigger("CapricornKnockBack");
+                CustomDebug.Log("<b>" + Caster.GetComponent<Player>().playerVitals.Name + "</b> should play <b><color=white>" + Name + "</color></b> attack animation", "Animation");
+
+                // set air cast to false
+                airCast = false;
+
+                // set spell on cooldown
+                SetCooldown();
+            }
+
+            // debug that spell is on cooldown
+            else { CustomDebug.Log("<b><color=white>" + Name + "</color></b> is on <color=blue>cooldown</color> for: <color=blue>" + CurrCooldown + "</color> sec", "Cooldown"); }
+        }
+
+        else
+        {
+            if (!usedInAir)
+            {
+                if (PlayerAbilitiesScript.GetCapricorn2Targets() != null)
+                {
+                    // set spell as started
+                    IsStarted = true;
+
+                    // set animation
+                    Caster.GetComponent<Animator>().SetTrigger("CapricornKnockUp");
+                    CustomDebug.Log("<b>" + Caster.GetComponent<Player>().playerVitals.Name + "</b> should play <b><color=white>" + Name + "2</color></b> attack animation", "Animation");
+
+                    // set air cast to false
+                    airCast = true;
+
+                    // reset TimeBetweenCasts
+                    TimeBeteewnCasts = 0;
+
+                    // reset cooldown
+                    SetCooldown();
+                }
+
+                // debug that spell is on cooldown
+                else { CustomDebug.Log("no target in range for <b><color=white>" + Name + "2</color></b>", "Spells"); }
+            }
+            else { CustomDebug.Log("<b><color=white>" + Name + "</color></b> is on <color=blue>cooldown</color> for: <color=blue>" + CurrCooldown + "</color> sec", "Cooldown"); }
+        }
+    }
+
+    public override void Cast()
+    {
+        // set spell as not started
+        IsStarted = false;
+
+        // set spell as cast
+        IsCast = true;
+
+        if (!airCast)
+        {
+            // cast spell
+            PlayerAbilitiesScript.castedMeeleSpell = this;
+            CustomDebug.Log("<b>" + Caster.GetComponent<Player>().playerVitals.Name + "</b> casted<b><color=white> " + Name + "</color></b>", "Spells");
+        }
+
+        else
+        {
+            if (PlayerAbilitiesScript.GetCapricorn2Targets() != null)
+            {
+                // use spell
+                UseCapricorn2(Caster, PlayerAbilitiesScript.GetCapricorn2Targets());
+                CustomDebug.Log("<b>" + Caster.GetComponent<Player>().playerVitals.Name + "</b> casted<b><color=white> " + Name + "2</color></b>", "Spells");
+            }
+        }
+    }
+
+    public override void AfterCast()
+    {
+        // set spell as not cast
+        IsCast = false;
+    }
+
+    public override void Use(GameObject _target)
+    {
+        // get the vitals of the target
+        LivingEntity Vitals = _target.GetComponent<Player>().playerVitals;
+
+        // knock the target up and deal damage
+        try { Vitals.ApplyKnockUp(height, this); }
+        catch (OverflowException) { Debug.LogError("Is Outside range of Int32 tyoe."); }
+
+        // deal damage
+        Vitals.GetDamage(Damage);
+    }
+
+    public void UseCapricorn2(GameObject _caster, GameObject _target)
+    {
+        // get the vitals of the target
+        LivingEntity targetVitals = _target.GetComponent<Player>().playerVitals;
+
+        // get distance between caster and target
+        float dirX = _target.transform.position.x - _caster.transform.position.x;
+        float dirY = _target.transform.position.y - _caster.transform.position.y;
+
+        // get mutiplicator
+        float multi = Mathf.Sqrt(1 / (dirX * dirX + dirY * dirY));
+
+        // kock target away form caster
+        float time = targetVitals.ApplyKnockUp(dirY * multi * knockBackStrenght, this);
+        Debug.Log("KnockUpTime: " + time);
+        targetVitals.ApplyKnockBack(dirX * multi * knockBackStrenght, time, this);
+
+        // deal damage
+        targetVitals.GetDamage(Damage);
+
+        // disable Capricorn2
+        usedInAir = true;
+    }
+
+    public override void UpdateCooldowns()
+    {
+        CurrCooldown -= Time.deltaTime;
+        if (CurrCooldown <= 0) { CurrCooldown = 0; IsDisabled = false; usedInAir = false; }
+    }
+}
---
+++ Assets/Scripts/Classes/Attacks.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Attacks {
+
+    private GameObject caster;
+    private LivingEntity playerVitals;
+    private PlayerAbilities playerAbilitiesScript;
+
+    private uint id;
+    private string name;
+    private string type;
+    private float damage;
+    private float castTime;
+    private float toTravelTime;
+    private float traveltTime;
+    private bool shallTravel = true;
+    private float duration;
+    private float maxCooldown;
+    private float currCooldown = 0;
+    private bool isDisabled = false;
+    private int durability;
+    private float range;
+    private int maxTargets;
+    private int playersHit;
+    private uint spellDir;
+    private bool isStarted = false;
+    private bool isCast = false;
+    private float timeBetweenCastes = 0;
+    private float bulletSpeed;
+
+    private GameObject[] hitTargets = new GameObject[4];
+    private Vector3 spellDirection = new Vector3(0, 90, 0);
+
+    //Empty Constructor
+    public Attacks() { }
+    
+    //Constructor with Heal
+    public Attacks(GameObject caster, uint id, string name, string type, int maxTargets, float damage, float castTime, float delay, float duration, float cooldown, float range, uint spellDir)
+    {
+        this.caster = caster;
+        this.playerVitals = caster.GetComponent<Player>().playerVitals;
+        this.playerAbilitiesScript = caster.GetComponent<PlayerAbilities>();
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.maxTargets = maxTargets;
+        this.damage = damage;
+        this.castTime = castTime;
+        this.toTravelTime = delay;
+        this.maxCooldown = cooldown;
+        this.range = range;
+        this.spellDir = spellDir;
+        if (GameManager._instance.abilityLimit != 0) { durability = GameManager._instance.abilityLimit; }
+        if (range > 0) { bulletSpeed = toTravelTime / range; }
+        else { bulletSpeed = toTravelTime; }
+    }
+
+    #region Get & Set 
+    public GameObject Caster { get { return caster; } }
+    public LivingEntity PlayerVitals { get { return playerVitals; } }
+    public PlayerAbilities PlayerAbilitiesScript { get { return playerAbilitiesScript; } }
+    public uint ID { get { return id; } }
+    public string Name { get { return name; } }
+    public string Type { get { return type; } }
+    public float Damage { get { return damage; } set { this.damage = value; } }
+    public float CastTime { get { return castTime; } }
+    public float ToTravelTime { get { return toTravelTime;  } }
+    public bool ShallTravel { get { return shallTravel; } set { shallTravel = value; } }
+    public float MaxCooldown { get { return maxCooldown; } }
+    public float CurrCooldown { get { return currCooldown; } set { this.currCooldown = value; } }
+    public bool IsDisabled { get { return isDisabled; } set { isDisabled = value; } }
+    public int Durability { get { return durability; } }
+    public float Range { get { return range; } set { range = value; } }
+    public int PlayersHit { get { return playersHit; } set { playersHit = value; } }
+    public uint SpellDir { get { return spellDir; } }
+    public bool IsCast { get { return isCast; } set { isCast = value; } }
+    public bool IsStarted { get { return isStarted; } set { isStarted = value; } }
+    public float TimeBeteewnCasts { get { return timeBetweenCastes; } set { timeBetweenCastes = value; } }
+    public float BulletSpeed { get { return bulletSpeed; } set { bulletSpeed = value; } }
+    public GameObject[] HitTargets { get { return hitTargets; } set { hitTargets = value; } }
+    public Vector2 SpellDirection { get { return spellDirection; } set { spellDirection = value; } }
+    #endregion
+
+    public abstract void StartSpell();
+
+    /// <summary> Casts spell and checks if spell can be casted </summary>
+    public abstract void Cast();
+
+    /// <summary> Used at button release </summary>
+    public abstract void AfterCast();
+
+    /// <summary> uses spell (deals damage, applys conditions) </summary>
+    public abstract void Use(GameObject _target);
+
+    /// <summary> checks if spell hits his maximum targets </summary>
+    public bool MaxTargetsReached()
+    {
+        if (maxTargets == 0) { return false; }
+        if (playersHit > maxTargets) { return true; }
+        return false;
+    }
+
+    public void ResetPlayersHit() { playersHit = 0; }
+
+    public float TravelDistance()
+    {
+        // if 'toTravelTime' is 0 it's instand and shall automaticly return max range
+        if (toTravelTime == 0) { traveltTime = toTravelTime; }
+
+        traveltTime += Time.deltaTime;
+
+        if (traveltTime >= toTravelTime)
+        {
+            traveltTime = 0;
+            shallTravel = false;
+            return range;
+        }
+
+        return range / toTravelTime * traveltTime;
+    }
+
+    public virtual void UpdateCooldowns()
+    {
+        currCooldown -= Time.deltaTime;
+        if (currCooldown <= 0) { currCooldown = 0; IsDisabled = false; /*Debug.Log(IsDisabled);*/ }
+    }
+
+    public virtual void SetCooldown()
+    {
+        currCooldown = maxCooldown;
+        isDisabled = true;
+    }
+}
