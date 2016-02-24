4334f81d6d5bc232e61f0a1af9af0d9f02adcd0c
bugiflo <bugiflo@gmail.com>
2016-02-24 16:07:06 +0100

    capricorn fix

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -46,7 +46,7 @@ public class GameManager : MonoBehaviour {
         DontDestroyOnLoad(gameObject);
         // set singelton instance
         if (_instance == null) { _instance = this; }
-        else { Debug.LogError("GameManager has already been instantiated"); Destroy(gameObject); }
+        else { Debug.LogWarning("GameManager has already been instantiated"); Destroy(gameObject); }
 
         // set playerNames length
         playerNames = new string[playerInfo.Length];
@@ -136,7 +136,7 @@ public class GameManager : MonoBehaviour {
             if (go != null)
             {
                 go.GetComponent<Player>().gamerulesDisabled = true;
-                go.GetComponent<Player>()._animator.SetTrigger("Spawn");
+                //go.GetComponent<Player>()._animator.SetTrigger("Spawn");
             }
         }
 
--- Assets/Scripts/RayCollider.cs
+++ Assets/Scripts/RayCollider.cs
@@ -1,130 +1,268 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RayCollider : MonoBehaviour {
+public class RayCollider : MonoBehaviour
+{
+    public struct directions
+    {
+        public directionBool value;
+        public directionGameObject gameObject;
+    }
+
+    public struct directionBool
+    {
+        public bool top;
+        public bool bottom;
+        public bool right;
+        public bool left;
+    }
+
+    public struct directionFloat
+    {
+        public float top;
+        public float bottom;
+        public float right;
+        public float left;
+    }
+
+    public struct directionGameObject
+    {
+        public GameObject top;
+        public GameObject bottom;
+        public GameObject right;
+        public GameObject left;
+    }
 
     private BoxCollider2D boxCollider;
 
-    public bool topEnable = true;
-    public bool bottomEnable = true;
-    public bool rightEnable = true;
-    public bool leftEnable = true;
+    public directionBool enable;
+    public directionFloat offset;
+    [HideInInspector] public directions collision;
 
+    [Header("Tag")]
+    public string checkTag = "";
+
+    [Header("Enable Collision")]
+    public bool enableTop = true;
+    public bool enableBottom = true;
+    public bool enableRight = true;
+    public bool enableLeft = true;
+
+    [Header("Offset")]
+    public float offsetTop = 0.0f;
+    public float offsetBottom = 0.0f;
+    public float offsetRight = 0.0f;
+    public float offsetLeft = 0.0f;
+
+    [Header("ShowRay")]
+    public bool showRays = false;
+    public Color rayColor = Color.red;
+
+    /*#region collisin bools
+    #region variables
+    private bool collision;
     private bool collisionTop;
-    private bool collisionBottom;
-    private bool collisionRight;
-    private bool collisionLeft;
+    private bool collision.value.bottom;
+    private bool collision.value.right;
+    private bool collision.value.left;
+    #endregion
 
-    private GameObject collisionbjectTop;
-    private GameObject collisionbjectBottom;
-    private GameObject collisionbjectRight;
-    private GameObject collisionbjectLeft;
+    #region getter
+    public bool Collision { get { return collision; } }
+    public bool CollisionTop { get { return collisionTop; } }
+    public bool collision.value.bottom { get { return collision.value.bottom; } }
+    public bool collision.value.right { get { return collision.value.right; } }
+    public bool collision.value.left { get { return collision.value.left; } }
+    #endregion
+    #endregion*/
+
+    /*#region collision GameObjects
+    #region variables
+    private GameObject collisionObject;
+    private GameObject collision.gameObject.top;
+    private GameObject collision.gameObject.bottom;
+    private GameObject collision.gameObject.right;
+    private GameObject collision.gameObject.left;
+    #endregion
+
+    #region getter
+    public GameObject CollisionObject { get { return collisionObject; } }
+    public GameObject collision.gameObject.top { get { return collision.gameObject.top; } }
+    public GameObject collision.gameObject.bottom { get { return collision.gameObject.bottom; } }
+    public GameObject collision.gameObject.right { get { return collision.gameObject.right; } }
+    public GameObject collision.gameObject.left { get { return collision.gameObject.left; } }
+    #endregion
+    #endregion*/
 
     void Awake()
     {
         boxCollider = gameObject.GetComponent<BoxCollider2D>();
     }
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-
-        if (topEnable)
+    void Update()
+    {
+        if (enableTop)
         {
-            TopCollider();
+            collision.gameObject.top = TopCollider();
+            collision.value.top = collision.gameObject.top != null;
         }
 
-        if (bottomEnable)
+        if (enableBottom)
         {
-            BottomCollider();
+            collision.gameObject.bottom = BottomCollider();
+            collision.value.bottom = collision.gameObject.bottom != null;
         }
 
-        if (rightEnable)
+        if (enableRight)
         {
-            RightCollider();
+            collision.gameObject.right = RightCollider();
+            collision.value.right = collision.gameObject.right != null;
         }
 
-        if (leftEnable)
+        if (enableLeft)
         {
-            LeftCollider();
+            collision.gameObject.left = LeftCollider();
+            collision.value.left = collision.gameObject.left != null;
         }
     }
 
-    private bool TopCollider()
+    private GameObject TopCollider()
     {
         // set origin
-        Vector2 origin = new Vector2(boxCollider.bounds.center.x - boxCollider.bounds.size.x / 2, boxCollider.bounds.center.y + boxCollider.bounds.size.y / 2);
+        Vector2 origin = new Vector2(boxCollider.bounds.center.x - boxCollider.bounds.size.x / 2, boxCollider.bounds.center.y + boxCollider.bounds.size.y / 2 + offsetTop);
         // set lenght
         float rayLenght = boxCollider.bounds.size.x;
 
         // draw debug line/ray
-        Debug.DrawRay(origin, Vector2.right * rayLenght, Color.red);
+        if (showRays) { Debug.DrawRay(origin, Vector2.right * rayLenght, rayColor); }
 
         // cast ray
         foreach (RaycastHit2D objectHit in Physics2D.RaycastAll(origin, Vector2.right, rayLenght))
         {
-            return true;
+            // get GameObject form ray
+            GameObject hit = objectHit.transform.gameObject;
+
+            // check if variable is not empty
+            if (hit != null)
+            {
+                // check if we should check the tag
+                if (checkTag != "")
+                {
+                    // check tag
+                    if (hit.tag == checkTag)    {   return hit.gameObject;  }
+                }
+
+                // return GameObject if we do not need to check the tag
+                else { return hit.gameObject; }
+            }
         }
 
-        return false;
+        // return null if there is no collision
+        return null;
     }
 
-    private bool BottomCollider()
+    private GameObject BottomCollider()
     {
         // set origin
-        Vector2 origin = new Vector2(boxCollider.bounds.center.x - boxCollider.bounds.size.x / 2, boxCollider.bounds.center.y - boxCollider.bounds.size.y / 2);
+        Vector2 origin = new Vector2(boxCollider.bounds.center.x - boxCollider.bounds.size.x / 2, boxCollider.bounds.center.y - boxCollider.bounds.size.y / 2 - offsetBottom);
         // set lenght
         float rayLenght = boxCollider.bounds.size.x;
 
         // draw debug line/ray
-        Debug.DrawRay(origin, Vector2.right * boxCollider.bounds.size.x, Color.red);
+        if (showRays) { Debug.DrawRay(origin, Vector2.right * boxCollider.bounds.size.x, rayColor); }
 
         // cast ray
         foreach (RaycastHit2D objectHit in Physics2D.RaycastAll(origin, Vector2.right, rayLenght))
         {
-            return true;
+            // get GameObject form ray
+            GameObject hit = objectHit.transform.gameObject;
+
+            // check if variable is not empty
+            if (hit != null)
+            {
+                // check if we should check the tag
+                if (checkTag != "")
+                {
+                    // check tag
+                    if (hit.tag == checkTag) { return hit.gameObject; }
+                }
+
+                // return GameObject if we do not need to check the tag
+                else { return hit.gameObject; }
+            }
         }
 
-        return false;
+        // return null if there is no collision
+        return null;
     }
 
-    private bool RightCollider()
+    private GameObject RightCollider()
     {
         // set origin
-        Vector2 origin = new Vector2(boxCollider.bounds.center.x + boxCollider.bounds.size.x / 2, boxCollider.bounds.center.y + boxCollider.bounds.size.y / 2);
+        Vector2 origin = new Vector2(boxCollider.bounds.center.x + boxCollider.bounds.size.x / 2 + offsetRight, boxCollider.bounds.center.y + boxCollider.bounds.size.y / 2);
         // set lenght
         float rayLenght = boxCollider.bounds.size.y;
 
         // draw debug line/ray
-        Debug.DrawRay(origin, Vector2.down * rayLenght, Color.red);
+        if (showRays) { Debug.DrawRay(origin, Vector2.down * rayLenght, rayColor); }
 
         // cast ray
         foreach (RaycastHit2D objectHit in Physics2D.RaycastAll(origin, Vector2.down, rayLenght))
         {
-            return true;
+            // get GameObject form ray
+            GameObject hit = objectHit.transform.gameObject;
+
+            // check if variable is not empty
+            if (hit != null)
+            {
+                // check if we should check the tag
+                if (checkTag != "")
+                {
+                    // check tag
+                    if (hit.tag == checkTag) { return hit.gameObject; }
+                }
+
+                // return GameObject if we do not need to check the tag
+                else { return hit.gameObject; }
+            }
         }
 
-        return false;
+        // return null if there is no collision
+        return null;
     }
 
-    private bool LeftCollider()
+    private GameObject LeftCollider()
     {
         // set origin
-        Vector2 origin = new Vector2(boxCollider.bounds.center.x - boxCollider.bounds.size.x / 2, boxCollider.bounds.center.y + boxCollider.bounds.size.y / 2);
+        Vector2 origin = new Vector2(boxCollider.bounds.center.x - boxCollider.bounds.size.x / 2 - offsetLeft, boxCollider.bounds.center.y + boxCollider.bounds.size.y / 2);
         // set lenght
         float rayLenght = boxCollider.bounds.size.y;
 
         // draw debug line/ray
-        Debug.DrawRay(origin, Vector2.down * rayLenght, Color.red);
+        if (showRays) { Debug.DrawRay(origin, Vector2.down * rayLenght, rayColor); }
 
         // cast ray
         foreach (RaycastHit2D objectHit in Physics2D.RaycastAll(origin, Vector2.down, rayLenght))
         {
-            return true;
+            // get GameObject form ray
+            GameObject hit = objectHit.transform.gameObject;
+
+            // check if variable is not empty
+            if (hit != null)
+            {
+                // check if we should check the tag
+                if (checkTag != "")
+                {
+                    // check tag
+                    if (hit.tag == checkTag) { return hit.gameObject; }
+                }
+
+                // return GameObject if we do not need to check the tag
+                else { return hit.gameObject; }
+            }
         }
 
-        return false;
+        // return null if there is no collision
+        return null;
     }
 }
